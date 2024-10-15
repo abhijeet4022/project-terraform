e2725ba8f1a236fb8d2266cc68aa521babadbfd0
abhijeet4022 <abhijeet4022@gmail.com>
2024-10-15 15:46:52 +0530

    Project Terraform: Installation file.

---
+++ install.sh
@@ -0,0 +1,8 @@
+echo -e "/e[1;36mTerraform Installation.\e[0m"
+
+echo -e "/e[31mSetup The Terraform Repository.\e[0m"
+sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
+
+echo -e "/e[31mInstall Terraform.\e[0m"
+sudo yum -y install terraform
