7a6c6fc192a61fb1fc7328c2c7f96228ef656da4
abhijeet4022 <abhijeet4022@gmail.com>
2024-10-15 16:02:48 +0530

    Project Terraform: Installation file.

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashi
 echo -e "\e[32mInstall Terraform.\e[0m"
 sudo yum -y install terraform
 
-if [ $? = 0 ]; then
+if [ $? -eq 0 ]; then
   echo -e "\e[1;32mInstallation Completed.\e[0m"
 else
   echo -e "\e[1;31mInstallation Failed.\e[0m"
