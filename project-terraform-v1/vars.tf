a93cab740d1fad17dc1bc19295eb740dc675e26c
Abhijeet Pramanik <abhijeet4022@gmail.com>
2024-10-16 14:13:38 +0530

    Project Terraform: vars.tf

---
+++ project-terraform-v1/vars.tf
@@ -0,0 +1,21 @@
+variable "ami" {
+  default     = "ami-0b4f379183e5706b9"
+  description = "CentOS 8 AMI"
+}
+variable "instance_type" {
+  default     = "t2.micro"
+  description = "Instance Capacity"
+}
+variable "sg" {
+  default     = ["sg-0243a82da6118ecb6"]
+  description = "`allow-all` SG ID."
+}
+
+# Replace The Zone ID for the Route 53 hosted zone.
+# The same domain name for the component will use in application config side.
+
+variable "zone_id" {
+  default = "Z03000202RXSEVOPTHMK1"
+  description = "The Zone ID for the Route 53 hosted zone."
+}
+
