761a0c5c8c47d42eb53a0c3d3c898d97c84049f8
abhijeet4022 <abhijeet4022@gmail.com>
2024-10-16 18:17:04 +0530

    Project Terraform: V2 Code.

--- project-terraform-v2/ec2.tf
+++ project-terraform-v2/ec2.tf
@@ -15,13 +15,3 @@ resource "aws_instance" "instance" {
   }
 
 }
-
-# This resource creates a DNS 'A' record for the component in Route 53.
-resource "aws_route53_record" "records" {
-  for_each = var.components
-  zone_id  = var.zone_id
-  name     = "${lookup(each.value, "name", null)}.learntechnology.cloud"
-  type     = "A"
-  ttl      = 30
-  records  = [lookup(lookup(aws_instance.instance, each.key, null ), "private_ip", null)]
-}
