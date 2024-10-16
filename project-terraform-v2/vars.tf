f192076e111f0d5a7de0d41754142cdb08f36d59
Abhijeet Pramanik <abhijeet4022@gmail.com>
2024-10-16 15:05:53 +0530

    Project Terraform: v2 vars file

--- project-terraform-v2/vars.tf
+++ project-terraform-v2/vars.tf
@@ -12,7 +12,24 @@ variable "sg" {
 # The same domain name for the component will use in application config side.
 
 variable "zone_id" {
-  default = "Z03000202RXSEVOPTHMK1"
+  default     = "Z03000202RXSEVOPTHMK1"
   description = "The Zone ID for the Route 53 hosted zone."
+}
+
+# Component Variables
+variable "components" {
+  description = "Map of application components with their names and instance types."
+  default     = {
+    frontend  = { name = "frontend-dev", instance_type = "t2.micro" }
+    mongodb   = { name = "mongodb-dev", instance_type = "t2.micro" }
+    catalogue = { name = "catalogue-dev", instance_type = "t2.micro" }
+    redis     = { name = "redis-dev", instance_type = "t2.micro" }
+    user      = { name = "user-dev", instance_type = "t2.micro" }
+    cart      = { name = "cart-dev", instance_type = "t2.micro" }
+    mysql     = { name = "mysql-dev", instance_type = "t2.micro" }
+    shipping  = { name = "shipping-dev", instance_type = "t3.small" }
+    rabbitmq  = { name = "rabbitmq-dev", instance_type = "t2.micro" }
+    payment   = { name = "payment-dev", instance_type = "t2.micro" }
+  }
 }
 
