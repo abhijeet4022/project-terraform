893c56fe2d0d70e61810646b655c85541e91653e
abhijeet4022 <abhijeet4022@gmail.com>
2024-10-16 19:17:50 +0530

    Project Terraform: V2 Code.

--- project-terraform-v3/module/infra/provisioner.tf
+++ project-terraform-v3/module/infra/provisioner.tf
@@ -2,8 +2,12 @@ resource "null_resource" "local-exec" {
   depends_on = [aws_route53_record.records]
   provisioner "local-exec" {
     command = <<EOF
-
-cd /home/centos/project-terraform/project-terraform-v3
+if [ -d /home/centos/project-ansible ]; then
+  cd /home/centos/project-ansible
+else
+  git clone https://github.com/abhijeet4022/project-ansible.git
+  cd /home/centos/project-ansible
+fi
 git pull
 sleep 60
 ansible-playbook -i ${var.name}-dev.learntechnology.cloud, main.yml -e ansible_user=centos -e ansible_user=DevOps321 -e component=${var.name}
